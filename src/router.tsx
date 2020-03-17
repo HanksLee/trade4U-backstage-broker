389d9a1789dc1fbe1f5555e5e8ae603473a3a3c7
TY <terenyeung@gmail.com>
2020-03-17 21:48:10 +0800

    [M] fixed up code style

--- src/router.tsx
+++ src/router.tsx
@@ -8,61 +8,61 @@ const routes: any[] = [
   {
     component: loadable(() =>
       import(/* webpackChunkName: "manager-page" */ "./pages/Manager")
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(/* webpackChunkName: "role-page" */ "./pages/Role")
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "account-page" */ "./pages/Account/AccountList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "transaction-page" */ "./pages/Account/TransactionList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-deposit-page" */ "./pages/Finance/Deposit/DepositList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-withdraw-page" */ "./pages/Finance/Withdraw/WithdrawList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-payment-page" */ "./pages/Finance/Payment/PaymentList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-rate-page" */ "./pages/Finance/Rate/RateList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "product-page" */ "./pages/Product/ProductList"
       )
-    )
+    ),
   },
   // {
   //   component: loadable(() =>
@@ -76,7 +76,7 @@ const routes: any[] = [
       import(
         /* webpackChunkName: "system-base-info-page" */ "./pages/System/SystemBaseInfo"
       )
-    )
+    ),
   },
   ,
   {
@@ -84,7 +84,7 @@ const routes: any[] = [
       import(
         /* webpackChunkName: "system-params-page" */ "./pages/System/SystemParams"
       )
-    )
+    ),
   }, // {
   //   component: loadable(() => import(/* webpackChunkName: "agency-log" */ './pages/Agency/AgencyLog/AgencyLogList')),
   // },
@@ -93,21 +93,21 @@ const routes: any[] = [
       import(
         /* webpackChunkName: "agency-log" */ "./pages/Agency/AgencyLog/AgencyLogList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "agency-info" */ "./pages/Agency/AgencyInfo/AgencyInfoList"
       )
-    )
+    ),
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "agency-agent" */ "./pages/Agency/AgencyMember/AccountList"
       )
-    )
+    ),
   }
 ];
 
