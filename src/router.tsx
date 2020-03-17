b6b94b2e827d44fe5c0bcb32f2ef7dcf797ef328
Hank Lee <baf6715@hotmail.com.tw>
2020-03-17 09:35:34 +0800

    router.tsx錯誤修改

--- src/router.tsx
+++ src/router.tsx
@@ -8,73 +8,75 @@ const routes: any[] = [
   {
     component: loadable(() =>
       import(/* webpackChunkName: "manager-page" */ "./pages/Manager")
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(/* webpackChunkName: "role-page" */ "./pages/Role")
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "account-page" */ "./pages/Account/AccountList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "transaction-page" */ "./pages/Account/TransactionList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-deposit-page" */ "./pages/Finance/Deposit/DepositList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-withdraw-page" */ "./pages/Finance/Withdraw/WithdrawList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-payment-page" */ "./pages/Finance/Payment/PaymentList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "finance-rate-page" */ "./pages/Finance/Rate/RateList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "product-page" */ "./pages/Product/ProductList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
-      import(/* webpackChunkName: "verify-page" */ "./pages/verify")
-    ),
+      import(
+        /* webpackChunkName: "verify-opne-account-page" */ "./pages/verify/OpenAccount"
+      )
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "system-base-info-page" */ "./pages/System/SystemBaseInfo"
       )
-    ),
+    )
   },
   ,
   {
@@ -82,7 +84,7 @@ const routes: any[] = [
       import(
         /* webpackChunkName: "system-params-page" */ "./pages/System/SystemParams"
       )
-    ),
+    )
   }, // {
   //   component: loadable(() => import(/* webpackChunkName: "agency-log" */ './pages/Agency/AgencyLog/AgencyLogList')),
   // },
@@ -91,21 +93,21 @@ const routes: any[] = [
       import(
         /* webpackChunkName: "agency-log" */ "./pages/Agency/AgencyLog/AgencyLogList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "agency-info" */ "./pages/Agency/AgencyInfo/AgencyInfoList"
       )
-    ),
+    )
   },
   {
     component: loadable(() =>
       import(
         /* webpackChunkName: "agency-agent" */ "./pages/Agency/AgencyMember/AccountList"
       )
-    ),
+    )
   }
 ];
 
