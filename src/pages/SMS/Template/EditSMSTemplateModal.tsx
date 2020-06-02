dd56d10fee463226adfda79ac0c070d3a823196e
Hank Lee <baf6715@hotmail.com.tw>
2020-06-02 15:31:27 +0800

    fix coding style

--- src/pages/SMS/Template/EditSMSTemplateModal.tsx
+++ src/pages/SMS/Template/EditSMSTemplateModal.tsx
@@ -10,8 +10,8 @@ import value from "*.json";
 
 const FormItem = Form.Item;
 const getFormItemLayout = (label, wrapper, offset?) => ({
-  labelCol: { span: label, offset },
-  wrapperCol: { span: wrapper }
+  labelCol: { span: label, offset, },
+  wrapperCol: { span: wrapper, },
 });
 
 interface IEditSMSTemplateModalProps {
@@ -30,11 +30,11 @@ interface IEditSMSTemplateModalState {
 @inject("common")
 @observer
 export default class EditSMSTemplateModal extends BaseReact<
-  IEditSMSTemplateModalProps,
-  IEditSMSTemplateModalState
+IEditSMSTemplateModalProps,
+IEditSMSTemplateModalState
 > {
   state = {
-    confirmLoading: false
+    confirmLoading: false,
     // editorContent: "",
   };
 
@@ -47,12 +47,12 @@ export default class EditSMSTemplateModal extends BaseReact<
   handleSubmit = async evt => {
     this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        const { smsTemplate, onOk } = this.props;
+        const { smsTemplate, onOk, } = this.props;
         // const { editorContent, } = this.state;
 
         let payload: any = {
           type: values.type,
-          status: values.status ? 1 : 0
+          status: values.status ? 1 : 0,
         };
 
         // if (!utils.isEmpty(editorContent)) {
@@ -64,7 +64,7 @@ export default class EditSMSTemplateModal extends BaseReact<
         }
 
         this.setState({
-          confirmLoading: true
+          confirmLoading: true,
         });
 
         if (!smsTemplate) {
@@ -75,7 +75,7 @@ export default class EditSMSTemplateModal extends BaseReact<
             },
             () => {
               this.setState({
-                confirmLoading: false
+                confirmLoading: false,
               });
             }
           );
@@ -87,7 +87,7 @@ export default class EditSMSTemplateModal extends BaseReact<
             },
             () => {
               this.setState({
-                confirmLoading: false
+                confirmLoading: false,
               });
             }
           );
@@ -97,8 +97,8 @@ export default class EditSMSTemplateModal extends BaseReact<
   };
 
   render() {
-    const { form, smsTemplate, onCancel } = this.props;
-    const { confirmLoading } = this.state;
+    const { form, smsTemplate, onCancel, } = this.props;
+    const { confirmLoading, } = this.state;
     const getFieldDecorator = form.getFieldDecorator;
     // const { TextArea } = Input;
 
@@ -115,20 +115,20 @@ export default class EditSMSTemplateModal extends BaseReact<
           <FormItem label="类型" {...getFormItemLayout(3, 12)} required>
             {getFieldDecorator("type", {
               initialValue: (smsTemplate && smsTemplate.type) || "",
-              rules: [{ required: true, message: "类型不能為空" }]
+              rules: [{ required: true, message: "类型不能為空", }],
             })(<Input placeholder="请输入类型" />)}
           </FormItem>
           {!smsTemplate && (
             <FormItem label="是否启用" {...getFormItemLayout(3, 12)}>
               {getFieldDecorator("status", {
                 initialValue: false,
-                valuePropName: "checked"
+                valuePropName: "checked",
               })(<Checkbox />)}
             </FormItem>
           )}
           <FormItem label="短信内容" {...getFormItemLayout(3, 12)}>
             {getFieldDecorator("content", {
-              initialValue: (smsTemplate && smsTemplate.content) || ""
+              initialValue: (smsTemplate && smsTemplate.content) || "",
               // rules: [{ required: true, message: "分類名稱不能為空" }]
             })(
               <Input placeholder="短信内容" />
